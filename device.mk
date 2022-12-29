feaf95b6832ca39c358c8b9cc1cb5cbdcfebfba1
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-12-29 16:10:31 +0100

    Spacewar: Enable blur is default

--- device.mk
+++ device.mk
@@ -185,6 +185,12 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
