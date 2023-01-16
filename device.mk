c1f06ec04da33b0c8eb21c32c7d6f2fb448abdf9
Jamal Rothfuchs <jamal2362@googlemail.com>
2023-01-16 14:17:39 +0100

    Disable NT Camera for now.

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
 
 # Camera
-$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
+# $(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
