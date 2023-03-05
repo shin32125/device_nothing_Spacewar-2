71d21059214d89d7ea9047eab8242aa1e5d76f03
shin32125 <shintaga32123@gmail.com>
2023-03-05 12:15:59 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -368,7 +368,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.3.vendor
+    android.hardware.power@1.3.vendor \
+    BoostFrameworkOverlay
 	
 # Perf Jars
 PRODUCT_BOOT_JARS += \
